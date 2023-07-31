37535597e8fcc8a502c18b848db906b4adb2ee47
Hunter7210 <heitorrmsilva@gmail.com>
2023-07-31 16:59:48 -0300

    31/07/2023

--- RevisaoFPOO/Agenda/Agendamento.java
+++ RevisaoFPOO/Agenda/Agendamento.java
@@ -2,23 +2,26 @@ package RevisaoFPOO.Agenda;
 
 public class Agendamento {
     
-    String dia;
-    String mes;
-    String ano;
+    String data;
+ /*    String mes; */
+/*     String ano; */
     String hora;
+    String dataHora;
+    
 
 
     public Agendamento(String dia, String mes, String ano, String hora) {
-        this.dia = dia;
-        this.mes = mes;
-        this.ano = ano;
+        /*   this.dia = dia;
+        this.mes = mes; */
+        /* this.ano = ano; */
         this.hora = hora;
     }
+
 
     public Agendamento() {
     }
 
-    public String getDia() {
+    /* public String getDia() {
         return dia;
     }
     public void setDia(String dia) {
@@ -29,18 +32,41 @@ public class Agendamento {
     }
     public void setMes(String mes) {
         this.mes = mes;
-    }
+    }/*  
     public String getAno() {
         return ano;
     }
     public void setAno(String ano) {
         this.ano = ano;
-    }
+    } */
     public String getHora() {
         return hora;
     }
+    public Agendamento(String dataHora) {
+        this.dataHora = dataHora;
+    }
+
     public void setHora(String hora) {
         this.hora = hora;
+    }
+
+    public String getDataHora() {
+        dataHora = data+hora;
+        return dataHora;
+    }
+
+    public void setDataHora(String dataHora) {
+        this.dataHora = dataHora;
+    }
+
+
+    public String getData() {
+        return data;
+    }
+
+
+    public void setData(String data) {
+        this.data = data;
     }
 
     
